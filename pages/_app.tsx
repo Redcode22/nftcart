4fd4fe6f8055ea821fd16b2ed4357dbd6bebb06f
Shoury Sharma <shanuu12e@gmail.com>
2022-07-11 22:15:39 +0530

    init: Boilerplate code

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+
+export const theme = extendTheme();
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 
 export default MyApp

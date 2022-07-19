7205aa987900a64b2f845b17b085662c5b750b91
Shoury Sharma <shanuu12e@gmail.com>
2022-07-19 16:48:31 +0530

    chore: Configure ts

---
+++ packages/contracts/src/Transactions.sol
@@ -0,0 +1,31 @@
+pragma solidity ^0.8.0;
+
+contract Transactions {
+    uint256 transactionCount;
+    event Transfer(address from, address receiver, uint amount, string message, uint256 timestamp, string keyword);
+    struct TransferStruct {
+        address sender;
+        address reciever;
+        uint amount;
+        string message;
+        uint256 timestamp;
+        string keyword;
+    }
+
+    TransferStruct[] transactions;
+
+    function addToBlockChain(address payable receiver, uint amount, string memory mssg, string memory keyword) public {
+        transactionCount++;
+        transactions.push(TransferStruct(msg.sender, receiver, amount, mssg, block.timestamp, keyword));
+        emit Transfer(msg.sender, receiver, amount, mssg, block.timestamp, keyword);
+    }
+
+    function getAllTransaction() public view returns (TransferStruct[] memory) {
+        return transactions;
+    }
+
+    function getTransactionCount() public view returns (uint256) {
+        return transactionCount;
+    }
+
+}
